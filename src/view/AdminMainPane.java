b7acb3373318c96d836332f56f2381276436fc00
Bo-vcu <bo.vancriekingeuyttendaele@student.ucll.be>
2022-11-22 14:17:41 +0100

    Update AdminMainPane.java

--- src/view/AdminMainPane.java
+++ src/view/AdminMainPane.java
@@ -4,14 +4,14 @@ package view;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
 import javafx.scene.layout.BorderPane;
-import view.panels.MetroCardOverviewPane;
+import view.panels.*;
 
 public class AdminMainPane extends BorderPane {
 	public AdminMainPane(){		
 	    TabPane tabPane = new TabPane(); 	    
         MetroCardOverviewPane metroCardOverviewPane = new MetroCardOverviewPane();
         Tab metroCardOverviewTab = new Tab("Metro cards overview",metroCardOverviewPane);
-        Tab controlCenterTab = new Tab("Control Center",metroCardOverviewPane);
+        Tab controlCenterTab = new Tab("Control Center", null);
         Tab setupTab = new Tab("Setup");
         tabPane.getTabs().add(controlCenterTab);
         tabPane.getTabs().add(metroCardOverviewTab);
