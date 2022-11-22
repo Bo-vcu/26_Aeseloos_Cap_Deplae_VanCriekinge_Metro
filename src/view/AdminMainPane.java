d1b0e46946094aa7146bb686a928b1c2b20db817
Bo-vcu <bo.vancriekingeuyttendaele@student.ucll.be>
2022-11-22 14:04:32 +0100

    setup push

---
+++ src/view/AdminMainPane.java
@@ -0,0 +1,21 @@
+package view;
+
+
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.layout.BorderPane;
+import view.panels.MetroCardOverviewPane;
+
+public class AdminMainPane extends BorderPane {
+	public AdminMainPane(){		
+	    TabPane tabPane = new TabPane(); 	    
+        MetroCardOverviewPane metroCardOverviewPane = new MetroCardOverviewPane();
+        Tab metroCardOverviewTab = new Tab("Metro cards overview",metroCardOverviewPane);
+        Tab controlCenterTab = new Tab("Control Center",metroCardOverviewPane);
+        Tab setupTab = new Tab("Setup");
+        tabPane.getTabs().add(controlCenterTab);
+        tabPane.getTabs().add(metroCardOverviewTab);
+        tabPane.getTabs().add(setupTab);
+        this.setCenter(tabPane);
+	}
+}
