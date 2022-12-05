093517d97c35db75ed12bbff276a063ed900f035
Gert-Aesloos <gert.aesloos@student.ucll.be>
2022-12-05 19:59:13 +0100

    kleine aanpassingen aan LoadSaveStrategyFactory.java

--- src/model/MetroFacade.java
+++ src/model/MetroFacade.java
@@ -2,12 +2,17 @@ package model;
 
 import model.database.loadSaveStrategies.LoadSaveStrategyFactory;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class MetroFacade {
     LoadSaveStrategyFactory loadSaveStrategyFactory = new LoadSaveStrategyFactory();
     public void openMetroStation(){
-        loadSaveStrategyFactory.createLoadSaveStrategy();
+        try {
+            loadSaveStrategyFactory.createLoadSaveStrategy();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
     }
     public ArrayList<Metrocard> getMetroCardList(){
--- src/model/database/loadSaveStrategies/LoadSaveStrategyFactory.java
+++ src/model/database/loadSaveStrategies/LoadSaveStrategyFactory.java
@@ -1,12 +1,14 @@
 package model.database.loadSaveStrategies;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
 public class LoadSaveStrategyFactory {
 
-    public LoadSaveStrategy createLoadSaveStrategy() {
+    public LoadSaveStrategy createLoadSaveStrategy() throws IOException {
         Properties properties = new Properties();
         InputStream is = new FileInputStream("src/bestanden/settings.properties");
 
