bb9bbd0c437e2f39847afd1b95d0cf9b9320e766
benjamindeplae <benjamin.deplae@student.ucll.be>
2022-12-18 16:18:20 +0100

    discount UI update

--- src/view/panels/MetroStationSetupPane.java
+++ src/view/panels/MetroStationSetupPane.java
@@ -27,11 +27,15 @@ public class MetroStationSetupPane extends GridPane {
         this.metroStationSetupPaneController = new MetroStationSetupPaneController(metro, this);
         VBox root = new VBox();
         root.setPadding(new Insets(5, 5, 5, 5));
+        this.setVgap(5);
+        this.setHgap(5);
 
         RadioButton button1 = new RadioButton("excel");
         button1.setUserData(new MetroCardsExcelLoadSaveStrategy());
+        button1.setPadding(new Insets(5, 0, 5, 0));
         RadioButton button2 = new RadioButton("txt");
         button2.setUserData(new MetrocardsTekstLoadSaveStrategy());
+        button2.setPadding(new Insets(5, 0, 5, 0));
         ToggleGroup group = new ToggleGroup();
         button1.setToggleGroup(group);
         if (metroStationSetupPaneController.read("database").contains("MetroCardsExcelLoadSaveStrategy")) button1.setSelected(true);
@@ -39,12 +43,16 @@ public class MetroStationSetupPane extends GridPane {
         if (metroStationSetupPaneController.read("database").contains("MetroCardsTekstLoadSaveStrategy")) button2.setSelected(true);
 
         CheckBox age64PlusDiscount = new CheckBox("64+ age discount");
+        age64PlusDiscount.setPadding(new Insets(5, 0, 5, 0));
         if (metroStationSetupPaneController.read("discount").contains("age64PlusDiscount")) age64PlusDiscount.setSelected(true);
         CheckBox christmasLeaveDiscount = new CheckBox("Christmas leave discount");
+        christmasLeaveDiscount.setPadding(new Insets(5, 0, 5, 0));
         if (metroStationSetupPaneController.read("discount").contains("christmasLeaveDiscount")) christmasLeaveDiscount.setSelected(true);
         CheckBox studentDiscount = new CheckBox("Student discount");
+        studentDiscount.setPadding(new Insets(5, 0, 5, 0));
         if (metroStationSetupPaneController.read("discount").contains("studentDiscount")) studentDiscount.setSelected(true);
         CheckBox frequentTravellerDiscount = new CheckBox("Frequent traveller discount");
+        frequentTravellerDiscount.setPadding(new Insets(5, 0, 5, 0));
         if (metroStationSetupPaneController.read("discount").contains("frequentTravellerDiscount")) frequentTravellerDiscount.setSelected(true);
 
         Button saveButton = new Button("save");
